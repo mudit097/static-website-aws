cf12e8dbbb881e75f92cdadf3a890c6d9a5ec5f4
Mudit Mathur <85672841+mudit097@users.noreply.github.com>
2024-12-29 13:57:05 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = 
+  profile = ""
+  # Configuration options
+}
